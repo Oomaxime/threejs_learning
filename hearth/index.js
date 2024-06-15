9d78ca23d2d32a958b9fbe82340718a3a2a18991
Maxime <maxime.bidan33@gmail.com>
2024-06-15 22:28:59 +0200

    first try on texture usage

--- hearth/index.js
+++ hearth/index.js
@@ -1,34 +1,52 @@
 import * as THREE from "three";
-import { OrbitControls } from "../libs/jsm143/controls/OrbitControls.js";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.Z = 5;
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+scene.add(earthGroup);
 new OrbitControls(camera, renderer.domElement);
-
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, 12);
 const material = new THREE.MeshStandardMaterial({
-  map: loader.load("./assets/"),
+  map: loader.load("./assets/daymap.jpg"),
 });
 
 const earthMesh = new THREE.Mesh(geometry, material);
-scene.add(eartMesh);
+earthGroup.add(earthMesh);
 
-const hemiLight = new THREE.hemiLight();
-scene.add(hemiLight);
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./assets/nightmap.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
+
+const cloudMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./assets/clouds.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const cloudMesh = new THREE.Mesh(geometry, cloudMat);
+cloudMesh.scale.setScalar(1.003);
+earthGroup.add(cloudMesh);
+
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
 
 function animate() {
   requestAnimationFrame(animate);
-
-  earthMesh.rotation.x += 0.01;
-  earthMesh.rotation.y += 0.01;
+  earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
+  cloudMesh.rotation.y += 0.002;
   renderer.render(scene, camera);
 }
 
